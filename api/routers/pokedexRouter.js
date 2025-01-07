2adc0f7b0390884cb9d0a49192c40e56ef88a4f4
Aj-Herrera-99 <ajherrera999@gmail.com>
2025-01-07 16:17:04 +0100

    card pokemon con le immagini

--- api/routers/pokedexRouter.js
+++ api/routers/pokedexRouter.js
@@ -7,17 +7,6 @@ router.get("/", index);
 
 function index(req, res) {
     if (!pokedex) return res.status(404).json("Not Found");
-    pokedex = pokedex.map((pokemon) => {
-        const imgPath =
-            pokemon.id < 10
-                ? `/public/pokedex/images/00${pokemon.id}.png`
-                : `/public/pokedex/images/0${pokemon.id}.png`;
-
-        return {
-            ...pokemon,
-            path: imgPath,
-        };
-    });
     res.json(pokedex);
 }
 
